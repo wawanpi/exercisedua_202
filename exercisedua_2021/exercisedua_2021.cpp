387115b192a384743963e0d1a560a9509bcfaacc
wawanpi <muhaguskurniawan@gmail.com>
2023-06-05 14:54:36 +0700

    Add project files.

---
+++ exercisedua_2021/exercisedua_2021.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Vector>
+using namespace std;
+
+class pengarang;
+class buku;
+class penerbit{
+	
+public:
+	string Nama;
+	vector<pengarang*> nama_pengarang;
+	buku(string pNama) :Nama(pNama) {
+		cout << ""
+
+	}
+
+
+	
+};
+
